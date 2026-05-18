c7a9083a14edbd9daa9fa0b38fe3468415e1d210
Diego <diego.izeppe2008@gmail.com>
2026-05-18 19:19:45 -0300

    fix: remove prisma external package, add npmrc

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,8 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "avatars.githubusercontent.com" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
-      { protocol: "https", hostname: "img.clerk.com" },
     ],
   },
-  serverExternalPackages: ["@prisma/client"],
 };
 
 export default nextConfig;
